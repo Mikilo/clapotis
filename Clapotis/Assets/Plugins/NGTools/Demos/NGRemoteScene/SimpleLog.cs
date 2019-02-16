1f785c25e96c585e8578964121ff54e9e4183fa2
Nguyen Michael <mikil0@yahoo.fr>
2019-02-15 22:15:02 -0500

    Introduction of Unity project.

---
+++ Clapotis/Assets/Plugins/NGTools/Demos/NGRemoteScene/SimpleLog.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NGTools.Tests
+{
+	public class SimpleLog : MonoBehaviour, IResolver
+	{
+		[Serializable]
+		public class AuxiliaryClass
+		{
+			public int		doubleNestedFieldInteger;
+			public int[]	doubleNestedArrayFieldInteger;
+			public Rect		rect;
+			//public RefClass	b;
+			//public Coo abc;
+		}
+
+		[Serializable]
+		public class Coo
+		{
+			//public AuxiliaryClass		nestedComplex;
+			//public Coo ab;
+			//public UnityEngine.Object	a;
+		}
+
+		[Serializable]
+		public class RefClass
+		{
+			public int					nestedFieldInteger;
+			public int[]				nestedArrayFieldInteger;
+			public AuxiliaryClass		nestedComplex;
+			public Coo					c;
+			//public UnityEngine.Object	a;
+		}
+
+		//public bool	dontDestroyOnLoad;
+
+		//public GUIStyle style;
+		//public AudioSource		audioSource1;
+		//public AudioSource audioSource2;
+		//public UnityEngine.Object		anyObject;
+		public UnityEngine.Object all2;
+		//public AudioSource	source1;
+		//public AudioSource	source2;
+		//public AudioSource	source3;
+		//public SimpleLog ref1;
+		public Component component;
+		//public Component	componentRef2;
+		//public MonoBehaviour	MonoBomponentRef1;
+		//public MonoBehaviour	MonoBomponentRef2;
+		//public IResolver interface1;
+		//public IResolver interface2;
+		//public GameObject	gameObject;
+		//public GameObject   unityObject;
+		public Texture texture;
+		public Texture2D texture2D;
+		public Sprite sprite;
+		//public Camera		camcam;
+		//public AudioListener AudioListener;
+		//public Projector	pro;
+		//public TrailRenderer	tr;
+		//public Shader		shaderr;
+		//public Material		mat1;
+		//public TextAsset	textAsset;
+		//public Material mat2;
+		//public SimpleLog log;
+		//public SimpleLog[] arrayLog;
+
+		//public UnityEngine.Object[] arrayObjects;
+
+		//public Vector2 vector2;
+		//public Vector3 vector3;
+		//public Vector4 vector4;
+		//public Quaternion quat;
+		public Color color;
+		public Rect rect;
+		public Bounds bounds;
+
+		//public Material		material { get; set; }
+		//public Material		sharedMaterial { get; set; }
+
+		//public string stringer;
+		public int fieldInteger;
+		//public int[] arrayFieldInteger;
+		public int refClas;
+		public RefClass refClass;
+		public RefClass[] arrayRefClass;
+		//public Color[] arrayColor;
+
+		//private AsyncOperation	loadScene;
+
+		private void	Awake()
+		{
+			//if (this.dontDestroyOnLoad == true)
+			//	GameObject.DontDestroyOnLoad(this);
+		}
+
+		protected virtual void OnEnable()
+		{
+			//if (this.fieldInteger > 0)
+			//{
+			//	this.all2 = new Mesh();
+			//	this.all2.name = "Mesh" + UnityEngine.Random.Range(0, 1000);
+			//}
+			//else
+			//{
+			//	this.texture2D = new Texture2D(2,2);
+			//	this.texture2D.name = "Texture" + UnityEngine.Random.Range(0, 1000);
+			//	this.texture2D.SetPixel(0, 1, new Color(0F, 0F, UnityEngine.Random.Range(0, 1F), 1F));
+			//	this.texture2D.Apply();
+			//}
+		}
+		/*
+		protected virtual void	OnGUI()
+		{
+			GUILayout.Label(SceneManager.GetActiveScene().name);
+
+			if (GUILayout.Button("Scene 0") == true)
+				SceneManager.LoadScene(0);
+			if (GUILayout.Button("Scene 1") == true)
+				SceneManager.LoadScene(1);
+			if (GUILayout.Button("Scene Async 0") == true)
+				this.loadScene = SceneManager.LoadSceneAsync(0);
+			if (GUILayout.Button("Scene Async 1") == true)
+				this.loadScene = SceneManager.LoadSceneAsync(1);
+
+			if (GUILayout.Button("Scene 0") == true)
+				SceneManager.LoadScene(0, LoadSceneMode.Additive);
+			if (GUILayout.Button("Scene 1") == true)
+				SceneManager.LoadScene(1, LoadSceneMode.Additive);
+			if (GUILayout.Button("Scene Async 0") == true)
+				this.loadScene = SceneManager.LoadSceneAsync(0, LoadSceneMode.Additive);
+			if (GUILayout.Button("Scene Async 1") == true)
+				this.loadScene = SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+
+			if (this.loadScene != null)
+			{
+				GUILayout.Label(this.loadScene.progress.ToString());
+				if (this.loadScene.isDone == true)
+				{
+					if (GUILayout.Button("Accept") == true)
+						this.loadScene.allowSceneActivation = true;
+				}
+			}
+		}*/
+
+		public void	Test()
+		{
+			Debug.Log("test");
+		}
+
+		float	OnHierarchyGUI(Rect r)
+		{
+			r.xMin = r.xMax - 20F;
+			GUI.Button(r, "b");
+			return r.xMin;
+		}
+
+		public void GetResolver(out int identifier, out Func<int, GameObject> resolver)
+		{
+			identifier = 20;
+			//resolver = a;
+			resolver = (int id ) => { return Camera.main != null ? Camera.main.gameObject : null; };
+		}
+
+		private GameObject	a(int id)
+		{
+			return Camera.main.gameObject;
+		}
+	}
+}
---
+++ Clapotis/Assets/Scripts/NetworkTest.cs
@@ -0,0 +1,47 @@
+﻿#pragma warning disable 618
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Clapotis
+{
+	public class NetworkTest : MonoBehaviour
+	{
+		public NetworkManager netManager;
+
+		public string	address;
+		public string	port;
+
+		private void	OnGUI()
+		{
+			if (GUILayout.Button("Snapshot netManager") == true)
+				NGDebug.Snapshot(this.netManager);
+
+			GUILayout.Label("Address");
+			this.address = GUILayout.TextField(this.address);
+			GUILayout.Label("Port");
+			this.port = GUILayout.TextField(this.port);
+
+			if (GUILayout.Button("Host") == true)
+			{
+				this.netManager.networkAddress = this.address;
+				this.netManager.networkPort = int.Parse(this.port);
+				this.netManager.StartHost();
+			}
+
+			if (GUILayout.Button("Connect") == true)
+			{
+				this.netManager.networkAddress = this.address;
+				this.netManager.networkPort = int.Parse(this.port);
+				this.netManager.StartClient();
+			}
+
+			if (this.netManager.isNetworkActive == true)
+			{
+				if (GUILayout.Button("StopHost") == true)
+					this.netManager.StopHost();
+				if (GUILayout.Button("StopClient") == true)
+					this.netManager.StopClient();
+			}
+		}
+	}
+}
